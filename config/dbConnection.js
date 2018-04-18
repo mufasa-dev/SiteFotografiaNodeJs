fa83679f2aca86cb8bed1dadfd5c2fa3b8903b93
Falcoaerobis <bruno.ribeiro96@hotmail.com.br>
2018-04-18 00:18:53 -0300

    Meu deploy em Node.JS na Umbler

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,37 @@
 
 var MongoModule = require('mongodb').MongoClient;
  
-const url = 'naboo.mongodb.umbler.com:42686';
+function dbConnection(){
+ this._MongoClient = undefined;
+ this._MongoDB = undefined;
+}
+
+dbConnection.prototype.connectToMongo = function(callback){
+ 
+	MongoModule.connect("mongo mongodb://falcoaerobis:deltora14@naboo.mongodb.umbler.com:42686/dbeliza", function(err, client){
+		var MongoClient = client;
+		var MongoDB = client;
+		  
+		return callback(MongoClient, MongoDB);
+	})
+
+  /*MongoModule.connect(url, function(err, client) {
+  
+  });*/
+ 
+}
+
+/*MongoModule.connect("mongo mongodb://falcoaerobis:deltora14@naboo.mongodb.umbler.com:42686/dbeliza", function(err, con){
+	if(err){return console.log(err)};
+	global.db = conn;
+})*/
+
+
+module.exports = function(){
+ return dbConnection;
+}
+
+/*const url = 'naboo.mongodb.umbler.com:42686';
 const dbName = 'dbeliza';
  
 function dbConnection(){
@@ -23,6 +53,6 @@ dbConnection.prototype.connectToMongo = function(callback){
  
 module.exports = function(){
  return dbConnection;
-}
+}*/
 
 //mongodb : // falcoaerobis:Deltor@14@ cluster0-shard-00-00-192m8.mongodb.net:27017,cluster0-shard-00-01-192m8.mongodb.net:27017,cluster0-shard-00-02- 192m8.mongodb.net:27017 / test ? Ssl = true & replicaSet = Cluster0-shard-0 & authSource = admin
